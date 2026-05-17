d7ac53fcb625a911212c5b108d53765b2a1b0f48
XerWo <realgamerex@gmail.com>
2026-05-17 22:46:43 +0200

    removed comment and catch

--- src/nn.zig
+++ src/nn.zig
@@ -1,12 +1,8 @@
-// src/nn_main.zig
 const std = @import("std");
 const nn = @import("nn/brain.zig");
 
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
 
-    nn.brain(allocator) catch |err| {
-        std.debug.print("{any}\n", .{err});
-        @panic("Brain initializing went wrong!");
-    };
+    try nn.brain(allocator);
 }
